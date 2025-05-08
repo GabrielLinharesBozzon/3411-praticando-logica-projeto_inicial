a920afd0da03c1610f3929451fcb2a0eb40232e2
GabrielLinharesBozzon <44560145+GabrielLinharesBozzon@users.noreply.github.com>
2025-05-07 21:48:42 -0300

    Adicionando arquivos Python das microatividades e trabalho prático

---
+++ estruturas_condicao1.py
@@ -0,0 +1,13 @@
+temperatura = 29
+
+if temperatura < 30:
+    print('A temperatura hoje está amena')
+else:
+    print('Hoje está fazendo calor')
+
+# Teste com temperatura = 31
+temperatura = 31
+if temperatura < 30:
+    print('A temperatura hoje está amena')
+else:
+    print('Hoje está fazendo calor') 
---
+++ estruturas_condicao2.py
@@ -0,0 +1,27 @@
+# Teste com tempoExperiencia = 5
+tempoExperiencia = 5
+
+if tempoExperiencia < 2:
+    print('Nível de conhecimento júnior.')
+elif tempoExperiencia > 2 and tempoExperiencia < 5:
+    print('Nível de conhecimento pleno.')
+else:
+    print('Nível de conhecimento sênior.')
+
+# Teste com tempoExperiencia = 1
+tempoExperiencia = 1
+if tempoExperiencia < 2:
+    print('Nível de conhecimento júnior.')
+elif tempoExperiencia > 2 and tempoExperiencia < 5:
+    print('Nível de conhecimento pleno.')
+else:
+    print('Nível de conhecimento sênior.')
+
+# Teste com tempoExperiencia = 3
+tempoExperiencia = 3
+if tempoExperiencia < 2:
+    print('Nível de conhecimento júnior.')
+elif tempoExperiencia > 2 and tempoExperiencia < 5:
+    print('Nível de conhecimento pleno.')
+else:
+    print('Nível de conhecimento sênior.') 
